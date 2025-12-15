331ae65b399c3c4ef634007cd577fee71afcf39a
GABETROLL <gabriel7icplhappy@gmail.com>
2025-12-15 16:24:03 -0400

    Add Cannon in D!

--- cannon_in_d.py
+++ cannon_in_d.py
@@ -1,21 +1,10 @@
-"""
-Continuously plays frequencies dictated in the terminal:
-
-new: <frequency to start playing>
-old: <frequency to stop playing>
-frame + frames - frequency_command.command_frame"""
-import sounddevice
-import numpy
-from matplotlib import pyplot
-from time import sleep
-from dataclasses import dataclass
 from music_player import *
 
 SAMPLE_RATE = 14400
 
 wave: numpy.ndarray = render_wave(
     Melody(
-        400,
+        480,
         [
             [288, 1728],
             [288, 1728],
@@ -112,49 +101,3 @@ wave: numpy.ndarray = render_wave(
     ),
     SAMPLE_RATE
 )
-current_frame: int = 0
-
-def sine_wave(start_frame: int, duration_in_frames: int, hz: float, amplitude: float) -> numpy.ndarray:
-    """
-    Assuming that a sine wave frequency of `hz` Hz has been playing at a "frame rate", or sample rate,
-    of `SAMPLE_RATE` for `start_frame` frames until now, this function returns the air pressure values for
-    the upcoming `duration_in_frames` frames.
-
-    The result should be returned as a numpy ndarray with shape `(duration_in_frames, 1)`.
-    """
-    return numpy.sin(
-        numpy.arange(start_frame, start_frame + duration_in_frames) / SAMPLE_RATE * 2 * numpy.pi * frequency
-    ) * amplitude
-
-
-def almost_zero(x: float):
-    """
-    Returns if `x` is within a certain distance to 0 in the number line.
-    """
-    return -0.01 < x < 0.01
-
-
-def callback(outdata: numpy.ndarray, frames: int, time, status) -> None:
-    """writes sound output to 'outdata' from sound_queue."""
-    global current_frame
-
-    outdata[:] = wave[current_frame:current_frame + frames].reshape((frames,1))
-    current_frame += frames
-
-
-def main():
-    print(f"wave: {wave.shape}")
-
-    stream = sounddevice.OutputStream(channels=1, samplerate=SAMPLE_RATE, callback=callback)
-    stream.start()
-
-    while True:
-        text = input(">>>")
-        if text == "q":
-            break
-
-    stream.stop()
-
-
-if __name__ == "__main__":
-    main()
--- music_player.py
+++ music_player.py
@@ -1,8 +1,9 @@
+import numpy
 from dataclasses import dataclass
 
 Hertz: type = float
 
-
+"""
 @dataclass
 class Fraction:
     numerator: int
@@ -35,24 +36,48 @@ class MusicAsRatios:
         self.notes = notes
 
 
-
-StartOrStopPlaying: type = bool
-
-START: StartOrStopPlaying = True
-STOP: StartOrStopPlaying = False
-
-
 @dataclass
-class FrequencyCommand:
-"""Describes when a frequency should either start or stop playing."""
+class PlayFrequency:
+    \"\"\"Describes when a frequency should start playing, and for how many frames.\"\"\"
     command_frame: int
-    command: StartOrStopPlaying
+    duration: int
     frequency: Hertz
     amplitude: float
 
 
 def transform_music(music_as_ratios: MusicAsRatios) -> list[FrequencyCommand]:
-    """
+    \"\"\"
     Reads music from ratios format, and returns it as a list of FrequencyCommand.
-    """
+    \"\"\"
     pass
+"""
+
+@dataclass
+class Melody:
+    beats_per_minute: float
+    notes: list[list[Hertz]]
+
+
+def render_wave(melody: Melody, sample_rate: int) -> numpy.ndarray:
+    beats_per_second: float = melody.beats_per_minute / 60
+    samples_per_beat_rounded: int = int(sample_rate / beats_per_second)
+
+    result: numpy.ndarray = numpy.zeros((samples_per_beat_rounded * len(melody.notes),))
+
+    for beat_index, beat in enumerate(melody.notes):
+        start_sample_index: int = beat_index * samples_per_beat_rounded
+        end_sample_index: int = start_sample_index + samples_per_beat_rounded
+
+        # start the frequency as it had always been playing
+        for frequency in beat:
+            print(result[start_sample_index:end_sample_index].shape)
+
+            beat_wave: numpy.ndarray = numpy.sin(
+                numpy.arange(start_sample_index, end_sample_index) / sample_rate * 2 * numpy.pi * frequency
+            )
+
+            print(beat_wave.shape)
+
+            result[start_sample_index:end_sample_index] += beat_wave
+
+    return result
