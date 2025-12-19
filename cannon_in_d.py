bd490d7728dd142cf334b5db222ec1a83ddbbc80
GABETROLL <gabriel7icplhappy@gmail.com>
2025-12-19 17:04:18 -0400

    Add new Cannon in D section

--- cannon_in_d.py
+++ cannon_in_d.py
@@ -19,7 +19,7 @@ C# => 1080
 """
 from music_player import *
 
-HOME = D3 = 144
+HOME = D3 = 288
 B2 = int(D3 * 5 / 6)
 G2 = int(D3 * 2 / 3)
 F_SHARP_3 = int(D3 * 5 / 4)
@@ -57,7 +57,7 @@ C_SHARP_6 = C_SHARP_4 << 2
 D6 = D3 << 3
 
 melody = Melody(
-    480,
+    360,
     [
         [D3],
         [D3],
@@ -281,6 +281,80 @@ melody = Melody(
 
         # 4
 
+        [D3, D5],
+        [D3, D5],
+        [A3, C_SHARP_5],
+        [A3, C_SHARP_5],
+        [D4, D5],
+        [D4, D5],
+        [A3, F_SHARP_4],
+        [A3, F_SHARP_4],
+
+        [A2, A4],
+        [A2, A4],
+        [E3, E4],
+        [E3, E4],
+        [A3, A4],
+        [A3, A4],
+        [E3, G4],
+        [E3, G4],
+
+        [B2, F_SHARP_4],
+        [B2, F_SHARP_4],
+        [F_SHARP_3, B4],
+        [F_SHARP_3, B4],
+        [B3, A4],
+        [B3, A4],
+        [F_SHARP_3, G4],
+        [F_SHARP_3, G4],
+
+        [F_SHARP_2, A4],
+        [F_SHARP_2, A4],
+        [C_SHARP_3, G4],
+        [C_SHARP_3, G4],
+        [F_SHARP_3, F_SHARP_4],
+        [F_SHARP_3, F_SHARP_4],
+        [C_SHARP_3, E4],
+        [C_SHARP_3, E4],
+
+        [G2, D4],
+        [G2, D4],
+        [D3, D4],
+        [D3, D4],
+        [G3, B4],
+        [G3, B4],
+        [D3, C_SHARP_5],
+        [D3, C_SHARP_5],
+
+        [D2, D5],
+        [D2, D5],
+        [A2, C_SHARP_5],
+        [A2, C_SHARP_5],
+        [D3, B4],
+        [D3, B4],
+        [A2, A4],
+        [A2, A4],
+
+        [G2, B4],
+        [G2, B4],
+        [D3, A4],
+        [D3, A4],
+        [G3, B4],
+        [G3, B4],
+        [D3, D5],
+        [D3, D5],
+
+        [A2, D5],
+        [A2, D5],
+        [E3, D5],
+        [E3, D5],
+        [A3, C_SHARP_5],
+        [A3, C_SHARP_5],
+        [E3, D5],
+        [E3, D5],
+
+        # 5
+
         [D3, A5],
         [D3, A5],
         [A3, F_SHARP_5],
@@ -503,15 +577,14 @@ melody = Melody(
     ],
 )
 
-"""print(list(
-    (index, (original, simplified))
-    for (index, (original, simplified)) in enumerate(zip(melody.notes, make_co_prime(melody.notes)))
-    if 11 in simplified
-))"""
 
-
-# print(C_SHARP_2, D2, E2, F_SHARP_2, G2, A2, B2, C_SHARP_3, D3, E3, F_SHARP_3, G3, A3, B3, C_SHARP_4, D4, E4, F_SHARP_4, G4, A4, B4, C_SHARP_5, D5, E5, F_SHARP_5, G5, A5, B5, C_SHARP_6, D6)
-
+print(C_SHARP_2, C_SHARP_3, C_SHARP_4, C_SHARP_5, C_SHARP_6)
+print(D2, D3, D5, D5, D6)
+print(E2, E3, E4, E5)
+print(F_SHARP_2, F_SHARP_3, F_SHARP_4, F_SHARP_5)
+print(G2, G3, G4, G5)
+print(A2, A3, A4, A5)
+print(B2, B3, B4, B5)
 
 wave: numpy.ndarray = render_wave(
     melody,
